62ce6cdf1b44079860ea3ad827fb15dedfd8ce07
sepusda <sepusda@pucesi.edu.ec>
2016-12-09 11:38:10 -0500

    primer

---
+++ AplicacionRed/AplicacionRed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacionRed.Startup))]
+namespace AplicacionRed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
